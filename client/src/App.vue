5d73850d923525c63e27ed49f2fcb1ab646ea0b3
gizmo49 <chikacheezy@gmail.com>
2018-10-23 17:13:57 +0100

    first

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+   
+    <posts></posts>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Posts from './components/Posts.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Posts
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
